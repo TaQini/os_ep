1f6a5a0ecb97b8a0bce0bc5e07871e00fd925514
TaQini <742954809@qq.com>
2018-05-08 10:35:11 +0800

    an update

--- ep6/src/ballistics/modeltest.c
+++ ep6/src/ballistics/modeltest.c
@@ -15,8 +15,9 @@
 int shmid;
 
 struct msg{
-	double t;
-	double r;
+	double current;
+	double next;
+	double range;
 }buf[101];
 
 void update(Result* rst, int i){
@@ -31,9 +32,19 @@ double showshm(int i){
 	struct msg *addr = (struct msg *)shmat(shmid, 0, 0);
 	double t;
 	t = addr[i].t;
-    printf("[%d] shm_state time: %f, %f \n", i, addr[i].t ,addr[i].r);
-    shmdt(addr);
+    	printf("[%d] shm_state time: %f, %f \n", i, addr[i].t ,addr[i].r);
+    	shmdt(addr);
 	return t;
+}
+
+void check(int a, int b){
+	if (showshm(a) <= showshm(b)){
+		printf("errrrrrrrrr!\n");
+		exit(0);
+	}
+	else{
+	//	printf("succccccccccess!!!!\n");
+	}
 }
 
 int main(){
@@ -43,32 +54,32 @@ int main(){
 
 	// create shm
 	shmid = shmget(SHMKEY, sizeof(buf), 0666| IPC_CREAT);
-	struct msg *addr = (struct msg *)shmat(shmid, 0, 0);
-	printf("successful!!!!!\n\n");
-	shmdt(addr);
+
+	// 
+	shmid_ds = shmget(SHMKEY_DS, sizeof(buf), 0666| IPC_CREAT);
+	// struct msg *addr = (struct msg *)shmat(shmid, 0, 0);
+	// printf("successful!!!!!\n\n");
+	// shmdt(addr);
 
 	pid_t pid;
 	if ((pid = fork())== 0){
-		Model * m = new Model(M);
+		Model * m = new Model(M); // the node who send msg
 		//while(rst=m->moveToNext()){
 	//		sleep(1);
+			printf("pred time: %f\n", m->getNextTime());
 			rst=m->moveToNext();
 			printf("Time %f Range %f [%d]\n", rst->getTime(), rst->getRange(), cnt++);
-
 			// send msg to shm
 			update(rst, 0);
-			if (showshm(0) <= showshm(1)){
-				printf("errrrrrrrrr!\n");
-				exit(0);
-			}else{
-				printf("succccccccccess!!!!\n");
-			}
+
+			// check err
+			check(0,1);
 
 			free(rst); // shit free
 		//}
 	}
 	else{
-		Model * m = new Model(N);
+		Model * m = new Model(N); // the node 
 		int i = 1;
 		//while(i){
 		//	i++;
@@ -81,6 +92,7 @@ int main(){
 			printf("\t\t\t\t\t[%d] Time %f Range %f\n", cnt++, rst->getTime(), rst->getRange());
 		//	if(i%4) 
 			update(rst, 1);
+			check(0,1);
 
 			free(rst); // shit free
 		}
